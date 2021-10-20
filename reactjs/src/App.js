2dbd84a52382131a26dbc9cb3dc1dec38ba9f867
SebastianMejiaVoid <deadbygravity@gmail.com>
2021-10-20 15:24:59 -0500

    implementé la función de agregar productos

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Product } from "./Product.js";
+import { UI } from "./UI.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// DOM Eventos
+document
+  .getElementById("product-form")
+  .addEventListener("submit", function (e) {
+    // Sobreescribe el comportamiento por defecto del form
+    e.preventDefault();
 
-export default App;
+    // Obtiene Los Valores De Los Formularios
+    const name = document.getElementById("name").value,
+      price = document.getElementById("price").value,
+      year = document.getElementById("id").value;
+
+    // Crea Nuevo Objeto Producto
+    const product = new Product(name, price, year);
+
+    // Crea Una Instancia UI
+    const ui = new UI();
+
+    // Input Validación de Usuario
+    if (name === "" || price === "" || year === "") {
+      ui.showMessage("Please Insert data in all fields", "danger");
+    }
+
+    // Guarda El Producto
+    ui.addProduct(product);
+    ui.showMessage("Product Added Successfully", "success");
+    ui.resetForm();
+  });
+
+document.getElementById("product-list").addEventListener("click", (e) => {
+  const ui = new UI();
+  ui.deleteProduct(e.target);
+  e.preventDefault();
+});
