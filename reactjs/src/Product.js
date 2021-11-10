7344805bbb83872d29a4f7e14b894ab81012d20d
Sebastian Mejia <deadbygravity@gmail.com>
2021-11-09 20:56:05 -0500

    implementé el atributo cantidad de la clase producto en el metodo contructor

--- reactjs/src/Product.js
+++ reactjs/src/Product.js
@@ -1,9 +1,10 @@
 // Constructor Producto
 export class Product {
-    constructor(name, price, id, state) {
+    constructor(name, price, id, state, canti) {
         this.name = name;
         this.price = price;
         this.id = id;
         this.state = state;
+        this.canti = canti;
     }
 }
